4bd5945768f2bd6d72a4d75002178cf3bbe3cf9c
cautus01 <69049801+cautus01@users.noreply.github.com>
2022-02-22 15:06:09 +0900

    [Bronze II] Title: 시험 감독, Time: 628 ms, Memory: 146204 KB -BaekjoonHub

--- 백준/Bronze/13458.시험 감독/시험 감독.py
+++ 백준/Bronze/13458.시험 감독/시험 감독.py
@@ -4,11 +4,9 @@ input=sys.stdin.readline
 n=int(input())
 a=list(map(int,input().split()))
 b,c=map(int,input().split())
-dp=[0]*1000005
-answer=0
+
+answer=n
 for i in a:
-    if i<=b:
-        answer+=1
-    else:
-        answer=answer+1+math.ceil((i-b)/c)
+    if i>b:
+        answer+=math.ceil((i-b)/c)
 print(answer)
