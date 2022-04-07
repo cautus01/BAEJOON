4c385bfde5ac363ff1ec4ce45007dd20e5634ad6
cautus01 <69049801+cautus01@users.noreply.github.com>
2022-04-07 19:44:22 +0900

    [Silver III] Title: N과 M (5), Time: 196 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,20 +1,8 @@
 import sys
+from itertools import permutations
 input=sys.stdin.readline
 
 n,m=map(int,input().split())
 data=sorted(list(map(int,input().split())))
-arr=[]
-isused=[0]*(n+1)
-def func(k):
-    if k==m:
-        print(*arr)
-        return
-    else:
-        for i in range(n):
-            if isused[i]==0:
-                isused[i]=1
-                arr.append(data[i])
-                func(k+1)
-                arr.pop()
-                isused[i]=0
-func(0)
+for com in list(permutations(data,m)):
+    print(*list(com))
