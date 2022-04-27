f6069a5efe4b1c6785548346077137d2ea6fba17
cautus01 <69049801+cautus01@users.noreply.github.com>
2022-04-27 15:55:55 +0900

    [Silver I] Title: 오르막 수, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -8,5 +8,5 @@ for i in range(10):
     d[1][i]=1
 for i in range(2,n+1):
     for j in range(10):
-        d[i][j]=sum([d[i-1][k] for k in range(j+1)])
+        d[i][j]=sum([d[i-1][k] for k in range(j+1)])%10007
 print(sum(d[n])%10007)
