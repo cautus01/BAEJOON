83399779f1ab97642b65dac0f100a6f3d458f1b8
cautus01 <69049801+cautus01@users.noreply.github.com>
2022-04-27 15:53:28 +0900

    [Silver I] Title: 오르막 수, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+d=[[0]*10 for _ in range(n+1)]
+
+for i in range(10):
+    d[1][i]=1
+for i in range(2,n+1):
+    for j in range(10):
+        d[i][j]=sum([d[i-1][k] for k in range(j+1)])
+print(sum(d[n])%10007)
